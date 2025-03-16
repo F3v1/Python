b7b2ae5a4faa52bb3b9b439e7812f6f622b69c66
Bro <fabian.Schlifter@gmail.com>
2025-03-16 18:31:45 +0100

    Create bruteforce.py
    A brute-force python password cracker.

---
+++ bruteforce.py
@@ -0,0 +1,20 @@
+import sys
+
+sys.stdout.reconfigure(line_buffering=True)
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.:,;_#'+*~<>|!§$%&/()=?´`@€µ²³{[]}\\"
+password_length = 3  #adjust the lenght of the passwords you want to create
+
+def generate_passwords(current_password, length):
+    if length == password_length:
+        print(current_password)
+        return
+
+    for char in alphabet:
+        generate_passwords(current_password + char, length + 1)
+
+def generate_all_passwords():
+    for i in range(password_length + 1):
+        generate_passwords("", i)
+
+generate_all_passwords()
